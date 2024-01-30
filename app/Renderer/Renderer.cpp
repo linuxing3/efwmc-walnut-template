4eb3edd1810f1679bc346f984a0be1cf7c2bf2fe
linuxing3 <linuxing3@qq.com>
2024-01-30 13:50:14 +0800

    [feat] image

--- app/Renderer/Renderer.cpp
+++ app/Renderer/Renderer.cpp
@@ -1,8 +1,9 @@
+#include <cstdint>
 #include <optional>
 
+#include "Walnut/Timer.h"
 #include "fmt/chrono.h"
 #include <glm/gtc/random.hpp>
-#include "Walnut/Timer.h"
 
 #include "Renderer/Ray.h"
 #include "Renderer/Renderer.h"
@@ -12,7 +13,28 @@
 
 #include <numeric>
 
+#define RENDER_PERQUAD
+
 namespace RTIAW::Render {
+
+static uint32_t ConvertToRGBA(const glm::vec4 &color) {
+  auto r = (uint8_t)(color.r * 255.0f);
+  auto g = (uint8_t)(color.g * 255.0f);
+  auto b = (uint8_t)(color.b * 255.0f);
+  auto a = (uint8_t)(color.a * 255.0f);
+
+  uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+  return result;
+}
+
+static void WriteColors(std::vector<uint8_t> m_renderBuffer, unsigned int idx,
+                        color pixel_color) {
+  m_renderBuffer[idx] = static_cast<uint8_t>(255.0f * pixel_color.r);
+  m_renderBuffer[idx + 1] = static_cast<uint8_t>(255.0f * pixel_color.g);
+  m_renderBuffer[idx + 2] = static_cast<uint8_t>(255.0f * pixel_color.b);
+  m_renderBuffer[idx + 3] = 255.0f;
+}
+
 Renderer::~Renderer() {
   switch (m_state) {
   case RenderState::Finished:
@@ -48,12 +70,15 @@ void Renderer::StopRender() { m_state = RenderState::Stopped; }
 
 std::vector<Renderer::Quad> Renderer::SplitImage(unsigned int quadSize) const {
   std::vector<Quad> result;
-  const unsigned int nX = static_cast<unsigned int>(std::ceil(m_imageSize.x / static_cast<float>(quadSize)));
-  const unsigned int nY = static_cast<unsigned int>(std::ceil(m_imageSize.y / static_cast<float>(quadSize)));
+  const auto nX = static_cast<unsigned int>(
+      std::ceil(m_imageSize.x / static_cast<float>(quadSize)));
+  const auto nY = static_cast<unsigned int>(
+      std::ceil(m_imageSize.y / static_cast<float>(quadSize)));
   for (unsigned int j = nY; j > 0; --j) {
     for (unsigned int i = 0; i < nX; ++i) {
-      result.emplace_back(glm::uvec2{i * quadSize, (j - 1) * quadSize},
-                          glm::min(glm::uvec2{(i + 1) * quadSize, j * quadSize}, m_imageSize));
+      result.emplace_back(
+          glm::uvec2{i * quadSize, (j - 1) * quadSize},
+          glm::min(glm::uvec2{(i + 1) * quadSize, j * quadSize}, m_imageSize));
     }
   }
 
@@ -66,6 +91,7 @@ void Renderer::Render() {
 
   m_state = RenderState::Running;
 
+#ifdef RENDER_PERLINE
   auto renderLine = [this](const unsigned int lineCoord) {
     if (m_state == RenderState::Stopped) {
       return;
@@ -75,14 +101,20 @@ void Renderer::Render() {
       const auto pixelCoord = glm::uvec2{i, lineCoord};
       color pixel_color{0, 0, 0};
       for (unsigned int i_sample = 0; i_sample < samplesPerPixel; ++i_sample) {
-        const auto u = (static_cast<float>(pixelCoord.x) + m_unifDistribution(m_rnGenerator)) / (m_imageSize.x - 1);
-        const auto v = (static_cast<float>(pixelCoord.y) + m_unifDistribution(m_rnGenerator)) / (m_imageSize.y - 1);
+        const auto u = (static_cast<float>(pixelCoord.x) +
+                        m_unifDistribution(m_rnGenerator)) /
+                       (m_imageSize.x - 1);
+        const auto v = (static_cast<float>(pixelCoord.y) +
+                        m_unifDistribution(m_rnGenerator)) /
+                       (m_imageSize.y - 1);
         Ray r = m_camera->NewRay(u, v);
         pixel_color += ShootRay(r, maxRayDepth);
       }
-      WritePixelToBuffer(pixelCoord.x, pixelCoord.y, samplesPerPixel, pixel_color);
+      WritePixelToBuffer(pixelCoord.x, pixelCoord.y, samplesPerPixel,
+                         pixel_color);
     }
   };
+#endif
 
   auto renderQuad = [this](const glm::uvec2 minCoo, const glm::uvec2 maxCoo) {
     if (m_state == RenderState::Stopped) {
@@ -95,38 +127,41 @@ void Renderer::Render() {
       for (unsigned int i = minCoo.x; i < maxCoo.x; ++i) {
         color pixel_color{0, 0, 0};
         const auto pixelCoord = glm::uvec2{i, j - 1};
-        for (unsigned int i_sample = 0; i_sample < samplesPerPixel; ++i_sample) {
-          const auto u = (static_cast<float>(pixelCoord.x) + m_unifDistribution(generator)) / (m_imageSize.x - 1);
-          const auto v = (static_cast<float>(pixelCoord.y) + m_unifDistribution(generator)) / (m_imageSize.y - 1);
+        for (unsigned int i_sample = 0; i_sample < samplesPerPixel;
+             ++i_sample) {
+          const auto u = (static_cast<float>(pixelCoord.x) +
+                          m_unifDistribution(generator)) /
+                         (m_imageSize.x - 1);
+          const auto v = (static_cast<float>(pixelCoord.y) +
+                          m_unifDistribution(generator)) /
+                         (m_imageSize.y - 1);
           Ray r = m_camera->NewRay(u, v);
           pixel_color += ShootRay(r, maxRayDepth);
         }
-        WritePixelToBuffer(pixelCoord.x, pixelCoord.y, samplesPerPixel, pixel_color);
+        WritePixelToBuffer(pixelCoord.x, pixelCoord.y, samplesPerPixel,
+                           pixel_color);
       }
     }
   };
 
-
   std::vector<std::future<void>> futures;
 
-//   #define RENDER_PERLINE
-  #ifdef RENDER_PERLINE
+#ifdef RENDER_PERLINE
   // Render per-line
   for (int j = m_imageSize.y - 1; j >= 0; --j) {
-   futures.push_back(m_threadPool.AddTask(renderLine, j));
+    futures.push_back(m_threadPool.AddTask(renderLine, j));
   }
-  #endif
-
-  #define RENDER_PERQUAD
-  #ifdef RENDER_PERQUAD
+#else
   // Render per-quad
   for (const auto &[minCoo, maxCoo] : SplitImage()) {
     futures.push_back(m_threadPool.AddTask(renderQuad, minCoo, maxCoo));
   }
-  #endif
+
+#endif
 
   // wait until all tasks are done...
-  std::for_each(begin(futures), end(futures), [](auto &future) { future.wait(); });
+  std::for_each(begin(futures), end(futures),
+                [](auto &future) { future.wait(); });
 
   Walnut::Timer timer;
   lastRenderTime = timer.ElapsedMillis();
@@ -136,9 +171,11 @@ void Renderer::Render() {
 color Renderer::ShootRay(const Ray &ray, unsigned int depth) {
   // If we've exceeded the ray bounce limit, no more light is gathered.
   if (depth == 0)
-    return color(0, 0, 0);
+    return {0, 0, 0};
 
-  if (const auto &[o_hitRecord, o_scatterResult] = m_scene.Hit(ray, 0.001f, RTIAW::Utils::infinity); o_hitRecord) {
+  if (const auto &[o_hitRecord, o_scatterResult] =
+          m_scene.Hit(ray, 0.001f, RTIAW::Utils::infinity);
+      o_hitRecord) {
     const auto &[t, p, normal, front_face] = o_hitRecord.value();
 
     if (o_scatterResult) {
@@ -156,19 +193,18 @@ color Renderer::ShootRay(const Ray &ray, unsigned int depth) {
   return (1.0f - t) * white + t * azure;
 }
 
-void Renderer::WritePixelToBuffer(unsigned int ix, unsigned int iy, unsigned int samples_per_pixel, color pixel_color) {
-  // flip the vertical coordinate because the display backend follow the opposite convention
-  // iy = m_imageSize.y - 1 - iy;
+void Renderer::WritePixelToBuffer(unsigned int ix, unsigned int iy,
+                                  unsigned int samples_per_pixel,
+                                  color pixel_color) {
+  // flip the vertical coordinate because the display backend follow the
+  // opposite convention iy = m_imageSize.y - 1 - iy;
 
   pixel_color /= samples_per_pixel;
   pixel_color = glm::sqrt(pixel_color);
   pixel_color = glm::clamp(pixel_color, 0.0f, 1.0f);
 
-  const unsigned int idx = 4 * (ix + iy * m_imageSize.x);
-  m_renderBuffer[idx] = static_cast<uint8_t>(255 * pixel_color.r);
-  m_renderBuffer[idx + 1] = static_cast<uint8_t>(255 * pixel_color.g);
-  m_renderBuffer[idx + 2] = static_cast<uint8_t>(255 * pixel_color.b);
-  m_renderBuffer[idx + 3] = 255;
+  unsigned int idx = 4 * (ix + iy * m_imageSize.x);
+  WriteColors(m_renderBuffer, idx, pixel_color);
 };
 
 } // namespace RTIAW::Render
